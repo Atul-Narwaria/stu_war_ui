940ac622775c4afe58e75f1fc2805d4a40b3af83
atul narwaria <58717956+Atul-Narwaria@users.noreply.github.com>
2023-08-14 00:25:33 +0530

    update api endpoint

--- src/service/instance.ts
+++ src/service/instance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:7000/api'
+    baseURL: 'https://stu-war-server.onrender.com/api'
 })
 const token = Cookies.get('token')
 instance.defaults.headers.common['Authorization'] = token;
